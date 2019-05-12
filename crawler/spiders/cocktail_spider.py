958afe1e620fe854420326fced5f1027c4b9fd5b
George Bezerra <gbezerra@gmail.com>
2019-05-12 17:02:17 +0200

    Pulled all cocktails

--- crawler/spiders/cocktail_spider.py
+++ crawler/spiders/cocktail_spider.py
@@ -23,6 +23,12 @@ class CocktailSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse_ingredient(self, text):
+
+        def parse_quantity(text):
+            expr = text.replace(' ', '+').replace('⁄', '/')
+            quantity = eval(expr)
+            return quantity
+
         text = text.replace('\xa0', ' ')
         patterns = [['oz', r"([\d\⁄\s]+)\s+oz\s+(.+)"],
                     ['tsp', r"([\d\⁄\s]+)\s+tsp\s+(.+)"],
@@ -33,6 +39,10 @@ class CocktailSpider(scrapy.Spider):
                     ['dashes', r"([\d\⁄\s]+)\s+(dashes|dash)\s+(.+)"],
                     ['top', r"\s*(.+),\s+to\s+(t|T)op"],
                     ['rinse', r"\s*(.+),\s+to\s+rinse"],
+                    ['mist', r"\s*(.+),\s+to\s+mist"],
+                    ['coating', r"\s*(.+),\s+for\s+coating"],
+                    ['drizzling', r"\s*(.+),\s+for\s+drizzling"],
+                    ['serving', r"\s*(.+),\s+for\s+serving"],
                     ['splash', r'.*Splash of (.+)'],
                     ['whole', r"([\d\⁄\s]+)\s+(.+)"],
                     ]
@@ -41,20 +51,25 @@ class CocktailSpider(scrapy.Spider):
             result = re.match(expr, text)
             if result:
                 if unit in ('oz', 'dashes', 'tsp', 'pinch', 'leaves', 'splashes', 'scoop'):
-                    ingredient['quantity'] = result.group(1).strip()
+                    ingredient['quantity'] = result.group(
+                        1).strip().replace('⁄', '/')
                     ingredient['item'] = result.group(2).strip()
                     ingredient['unit'] = unit
-                elif unit in ('top', 'splash', 'rinse'):
+                elif unit in ('top', 'splash', 'rinse', 'coating', 'drizzling', 'serving', 'mist'):
                     ingredient['quantity'] = None
                     ingredient['item'] = result.group(1).strip()
                     ingredient['unit'] = unit
                 elif unit in ('whole'):
-                    ingredient['quantity'] = result.group(1).strip()
+                    ingredient['quantity'] = result.group(
+                        1).strip().replace('⁄', '/')
                     ingredient['item'] = result.group(2).strip()
                     ingredient['unit'] = None
                 break
-        # if 'quantity' not in ingredient:
-        #    pdb.set_trace()
+
+        if 'quantity' not in ingredient:
+            ingredient['quantity'] = None
+            ingredient['item'] = text.split(',')[0].strip()
+            ingredient['unit'] = None
         print('ingredient:', ingredient)
         return ingredient
 
@@ -62,6 +77,7 @@ class CocktailSpider(scrapy.Spider):
         # parse cocktail name
         name = response.css('div.col-xs-12').css('h1').get()
         name = BeautifulSoup(name).get_text()
+        print('cocktail name:', name)
 
         # parse ingredients
         ingredient_list = []
@@ -71,9 +87,32 @@ class CocktailSpider(scrapy.Spider):
             print(text)
             ingredient_list.append(self.parse_ingredient(text))
 
+        # parse garnish
+        garnish_list = []
+        for garnish in set(response.css('div.row.x-recipe-garnish').css('span.oz-value').getall()):
+            soup = BeautifulSoup(garnish)
+            text = soup.get_text()
+            print('Garnish:', text)
+            garnish_list.append(text.strip())
+
+        # parse glass
+        glass = response.css('div.row.x-recipe-glasstype').css('a::text').get()
+
+        # parse preparation
+        preparation = '\n'.join(response.css(
+            'div.row.x-recipe-prep').css('p::text').getall())
+
+        # parse image url
+        image_url = response.css(
+            'img.wp-post-image.img-responsive::attr(src)').get()
+
         # write contents to file
         cocktail = {'name': name,
-                    'ingredients': ingredient_list}
+                    'ingredients': ingredient_list,
+                    'garnish': garnish_list,
+                    'glass': glass,
+                    'preparation': preparation,
+                    'imageURL': image_url}
         with open('../output/' + name + '.json', 'w') as f:
             json.dump(cocktail, f)
 
